355150f5de57e68f19a01b6d77d13d3b3eae6e43
ChandanKSahu <chandanksahu93@gmail.com>
2026-05-18 23:19:52 -0400

    Always show all nav links in header: disable greedy-nav, use flex-wrap

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -1,72 +1,13 @@
 /*
-* Greedy Navigation
+* Greedy Navigation - DISABLED
 *
-* http://codepen.io/lukejacksonn/pen/PwmwWV
-*
+* All nav links are always visible via CSS flex-wrap.
+* The overflow-to-dropdown behaviour has been removed so
+* every header link stays in the masthead at all times.
 */
 
-var $nav = $('#site-nav');
-var $btn = $('#greedy-nav-btn');
-var $vlinks = $('#site-nav .visible-links');
-var $hlinks = $('#site-nav .hidden-links');
-
-var breaks = [];
-
-function updateNav() {
-
-  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
-
-  // The visible list is overflowing the nav
-  if($vlinks.width() > availableSpace) {
-
-    // Record the width of the list
-    breaks.push($vlinks.width());
-
-    // Move item to the hidden list
-    $vlinks.children('*:not(.masthead__menu-item--lg)').last().prependTo($hlinks);
-
-    // Show the dropdown btn
-    if($btn.hasClass('hidden')) {
-      $btn.removeClass('hidden');
-    }
-
-  // The visible list is not overflowing
-  } else {
-
-    // There is space for another item in the nav
-    if(availableSpace > breaks[breaks.length-1]) {
-
-      // Move the item to the visible list
-      $hlinks.children().first().appendTo($vlinks);
-      breaks.pop();
-    }
-
-    // Hide the dropdown btn if hidden list is empty
-    if(breaks.length < 1) {
-      $btn.addClass('hidden');
-      $hlinks.addClass('hidden');
-    }
-  }
-
-  // Keep counter updated
-  $btn.attr("count", breaks.length);
-
-  // Recur if the visible list is still overflowing the nav
-  if($vlinks.width() > availableSpace) {
-    updateNav();
-  }
-
-}
-
-// Window listeners
-
-$(window).resize(function() {
-  updateNav();
+// Hide the dropdown button and ensure hidden-links list stays empty
+$(function() {
+  $('#greedy-nav-btn').addClass('hidden');
+  $('#site-nav .hidden-links').addClass('hidden');
 });
-
-$btn.on('click', function() {
-  $hlinks.toggleClass('hidden');
-  $(this).toggleClass('close');
-});
-
-updateNav();
